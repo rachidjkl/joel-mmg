6c3880997644f7f6f47463f84c7da559e60d30a4
rachidjkl <rghenema2122@politecnics.barcelona>
2023-02-08 21:15:16 +0100

    dsds

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.awt.Color.red;
+
 public class Main {
     public static void main(String[] args) {
-        Usuario user = new Usuario();
-        int eleccion;
 
+        Usuario user = new Usuario();
+        ArrayList<Usuario> mmg = new ArrayList<>();
+        cargarUsuarios(mmg);
+        int eleccion;
 
         do {
             System.out.println("ACCÉS A LA GESTIÓ DE JOCS\n  1. Registre\n 2. Accedir\n  0. Sortir\n ");
@@ -15,21 +19,19 @@ public class Main {
             {
                 switch (eleccion) {
                     case 1:
-                        registrar(user);
+                        registrar(user, mmg);
                         break;
                     case 2:
-                        cargarUsuarios();
+                        cargarUsuarios(mmg);
                         break;
                 }
-
             }
-
-        }while (eleccion != 3);
-
+        } while (eleccion != 3);
     }
 
-    private static void registrar(Usuario user) {
-        boolean contin = true;
+    private static void registrar(Usuario user, ArrayList<Usuario> mmg) {
+
+        boolean contin;
         Scanner sc = new Scanner(System.in);
         do {
             System.out.println("Nombre de usuario:");
@@ -46,21 +48,29 @@ public class Main {
             user.cuenta = sc.nextLine();
             System.out.println("Email:");
             user.email = sc.nextLine();
-            contin = comprovacion(user);
-        }while(contin);
+            contin = comprovacion(user, mmg);
+        } while (contin);
         guardarTxt(user);
-
     }
 
-    private static boolean comprovacion(Usuario user) {
+    private static boolean comprovacion(Usuario user, ArrayList<Usuario> mmg ) {
+
         boolean cont = false;
-        if(user.usuario == "mmg"){
-            cont = true;
+        for (int i=0;i<mmg.size() && cont == false;i++){
+            if (user.usuario.equals(mmg.get(i).usuario)) {
+                cont = true;
+                System.out.println("Usuario existente");
+            }
+            if (user.email.equals(mmg.get(i).email)) {
+                cont = true;
+                System.out.println("Email existente");
+            }
         }
-        System.out.println("mmg");
-    return cont;
+        return cont;
     }
+
     private static void guardarTxt(Usuario user) {
+
         try {
             FileWriter writer = new FileWriter("ADMIN/users.txt", true);
             writer.write(user.usuario + ":" + user.contra + ":" + user.nombre + ":" + user.apellido + ":" + "ES" + user.cuenta + ":" + user.email + ":0" + "\n");
@@ -71,12 +81,9 @@ public class Main {
         }
     }
 
-    private static void cargarUsuarios() {
+    private static void cargarUsuarios(ArrayList<Usuario> mmg) {
 
         BufferedReader reader = null;
-
-        ArrayList<Usuario> mmg = new ArrayList<>();
-
         try {
             reader = new BufferedReader(new FileReader("ADMIN/users.txt"));
             String line;
@@ -84,10 +91,7 @@ public class Main {
             String absolute = f.getAbsolutePath();
             FileReader fr = new FileReader(absolute);
             BufferedReader br = new BufferedReader(fr);
-            int mmgg = (int) br.lines().count();
-
-
-            for (int i = 0; i < mmgg; i++) {
+            for (int i = 0; i < br.lines().count(); i++) {
                 while ((line = reader.readLine()) != null) {
                     Usuario user = new Usuario();
                     String[] fields = line.split(":");
@@ -98,11 +102,9 @@ public class Main {
                     user.cuenta = fields[4];
                     user.email = fields[5];
                     mmg.add(user);
-
                 }
             }
             reader.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
