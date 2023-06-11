49f5a4be1fc0e38ad9d674a35cbf03bb68dd5c1c
rachidjkl <rghenema2122@politecnics.barcelona>
2023-06-11 18:02:39 +0200

    s

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Cliente {
+    public static void main(String[] args) {
+        String servidor = "localhost";
+        int puerto = 1234;
+
+        try {
+            // Crear el socket del cliente y establecer la conexión
+            Socket clientSocket = new Socket(servidor, puerto);
+            System.out.println("Conexión establecida con el servidor");
+
+            // Crear los streams de entrada y salida
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            // Enviar mensaje al servidor
+            String mensaje = "¡Hola servidor!";
+            output.println(mensaje);
+
+            // Leer respuesta del servidor
+            String respuestaServidor = input.readLine();
+            System.out.println("Servidor responde: " + respuestaServidor);
+
+            // Cerrar conexiones
+            input.close();
+            output.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,2 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+    public static void main(String[] args) {
+        int puerto = 1234;
+
+        try {
+            // Crear el socket del servidor
+            ServerSocket serverSocket = new ServerSocket(puerto);
+            System.out.println("Servidor escuchando en el puerto " + puerto);
+
+            // Esperar a que un cliente se conecte
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
+
+            // Crear los streams de entrada y salida
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            // Leer datos del cliente y enviar respuesta
+            String mensajeCliente = input.readLine();
+            System.out.println("Cliente dice: " + mensajeCliente);
+
+            String respuesta = "Hola cliente, ¡conexión establecida!";
+            output.println(respuesta);
+
+            // Cerrar conexiones
+            input.close();
+            output.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
